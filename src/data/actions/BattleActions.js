f68441c9345453835571c3777016bd63db6f00f3
Baum Digital <baumdigital@Mac-Mini.local>
2017-06-16 11:20:31 -0600

    fix

--- src/data/actions/BattleActions.js
+++ src/data/actions/BattleActions.js
@@ -13,12 +13,15 @@ export const setPj2 = ( payload ) => ({ type: SETPJ_2, payload });
 export const endBattle = ( payload ) => ({ type: END_BATTLE, payload });
 
 export const startBattle = (pj1ID,pj2ID) => {
-	if( pj1ID && pj2ID ){
-		dispatch({
-			type : START_BATTLE
-		});
-		Router.simulador();
-	} else {
-		alert('Debe Seleccionar los 2 personajes para continuar!');
+
+	return (dispatch) => {
+			if( pj1ID && pj2ID ){
+				dispatch({
+					type : START_BATTLE
+				});
+				Router.simulation();
+			} else {
+				alert('Debe Seleccionar los 2 personajes para continuar!');
+			}
 	}
 };
